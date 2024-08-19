7a0de0a219dc4b90a0e2be168803ca97edd34d98
raidghani12 <raidghani@gmail.com>
2024-08-19 06:46:29 +0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,82 +46,6 @@
   });
 
   /**
-   * Masonry Gallery
-   */
-  (function ($) {
-    $.fn.masonryFilter = function (options) {
-      var reload = function ($container) {
-        setTimeout(function () {
-          $container.masonry("layout");
-        }, 100);
-      };
-
-      var process = function ($container) {
-        var items = $container.masonry("getAllItems"),
-          revealItems = [],
-          hideItems = [];
-
-        $.each(items, function (i) {
-          var item = items[i];
-          var elm = $(item.element),
-            shouldShow = options.filter && options.filter.call(elm);
-
-          if (shouldShow) {
-            if (item.isHidden) {
-              item.isIgnored = false;
-              revealItems.push(item);
-            }
-          } else {
-            if (!item.isHidden) {
-              item.isIgnored = true;
-              hideItems.push(item);
-            }
-          }
-        });
-
-        $container.masonry('hide', hideItems);
-        $container.masonry('reveal', revealItems);
-        reload($container);
-      };
-
-      return this.each(function () {
-        var self = $(this);
-        process(self);
-      });
-    };
-  }(window.jQuery));
-
-  /**
-   * Initialize Masonry for the first tab
-   */
-  $(function () {
-    var self = $("#masonry-parent");
-
-    self.imagesLoaded(function () {
-      self.masonry({
-        gutterWidth: 15,
-        isAnimated: true,
-        itemSelector: ".card-masonry-item"
-      });
-    });
-
-    // Reinitialize Masonry for the second tab
-    $('a[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
-      var target = $(e.target).attr("href");
-      if (target === '#menu-4') {
-        var masonryParent1 = $("#masonry-parent1");
-        masonryParent1.imagesLoaded(function () {
-          masonryParent1.masonry({
-            gutterWidth: 15,
-            isAnimated: true,
-            itemSelector: ".card-masonry-item"
-          });
-        });
-      }
-    });
-  });
-
-  /**
    * Display random content
    */
   const contentIds = ['content1', 'content2'];
@@ -155,5 +79,31 @@
 
   window.addEventListener('load', toggleScrollTop);
   document.addEventListener('scroll', toggleScrollTop);
+
+  (function() {
+    "use strict";
+  
+    // Other functionalities remain the same...
+  
+    /**
+     * Remove specific CSS class on small screens
+     */
+    function removeClassOnSmallScreen() {
+      if (window.innerWidth <= 768) { // Adjust the width as needed for "small" screens
+        console.log('Screen is small, removing class...');
+        document.querySelectorAll('.m--75').forEach(function(element) {
+          console.log('Removing class from:', element);
+          element.classList.remove('m--75');
+        });
+      } else {
+        console.log('Screen is not small, class not removed.');
+      }
+    }
+  
+    window.addEventListener('load', removeClassOnSmallScreen);
+    window.addEventListener('resize', removeClassOnSmallScreen);
+  
+  })();
+  
 
 })();
